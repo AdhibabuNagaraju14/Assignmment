02ebfb778177641430e78fae26c15c7e81585c96
AdhibabuNagaraju14 <129249822+AdhibabuNagaraju14@users.noreply.github.com>
2023-03-29 14:45:22 +0530

    out put

---
+++ assigment1.1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int nagaraju [10];
+	printf("enter the 10 numbers: ");
+       for(int i=0;i<10;++i);
+	scanf("%d",&nagaraju[10]);
+	printf("display intager");
+		for(int j=0;j<10;++j)
+	scanf("%d",&nagaraju[10]);
+return 0;
+}
